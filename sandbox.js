114e7260a7e8e1a42279b3c8e8f73459eef2531b
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-28 16:46:18 +0600

    callbackfunction

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,15 @@
-//create a request object
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+  
+  //create a request object
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
+  request.addEventListener('readystatechange', () => {
   //console.log(request, request.readyState);
   if(request.readyState === 4  && request.status === 200){
-    console.log(request, request.responseText);
+    callback(undefined, request.responseText);
   }
   else if(request.readyState === 4) {
-    console.log('could not fetch the data');
+    callback('could not fetch data', undefined);
   }
   
 }) 
@@ -15,8 +17,29 @@ request.addEventListener('readystatechange', () => {
 //'readystatechange': event
 
 //open(): used for setting request 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
 //open('type of request', 'where to make request')
 
 //send request
 request.send();
+
+} 
+ //asynchornous behavior test
+console.log(1);
+console.log(2);
+
+getTodos( (err, data)=> {
+  console.log('callback fired');
+  if(err) {
+    console.log(err);
+  }
+  else {
+    console.log(data);
+  }
+});
+
+ //asynchornous behavior test
+ console.log(3);
+ console.log(4);
+ 
+
