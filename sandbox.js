114bf172230534b56dfa6b795438d33188738e36
MirajHoque <a.h.miraj2031@gmail.com>
2021-08-01 01:47:47 +0600

    fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,11 @@
-const getTodos = (resource) => {
-  return new Promise( (resolve, reject) => {
-    
-    const request = new XMLHttpRequest();
-
-  request.addEventListener('readystatechange', () => {
-  if(request.readyState === 4  && request.status === 200){
-    const data = JSON.parse(request.responseText)
-    resolve(data);
-  }
-  else if(request.readyState === 4) {
-    reject('error getting resource')
-  }
-  
-}) 
-
-request.open('GET', resource)
-request.send();
-  })
-  
-} 
-
-getTodos('todos/luigi.json').then( (data) => {
-  console.log('promise 1 resolved', data);
-  return getTodos('todos/mario.json')
+fetch ('todos/luigi.json').then( (response) => {
+  console.log('resolved', response) //response(object)
+  return response.json() //returns a promise
+  // json() used for get actual data
 }).then( (data) => {
-  console.log('promise 2 resolved', data);
-  return getTodos('todos/shaun.json')
-}).then( (data) => {
-  console.log('promise 3 resoved', data)
+  console.log(data);
 }).catch( (err) => {
-  console.log(err);
+  console.log('rejected', err);
 })
 
  
